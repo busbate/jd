906fbf4fab6a8f01b4c3b3ca76b894c985faba93
busbate <busbater@protonmail.com>
2021-06-23 14:25:25 +0800

    Update jd_joy_new.js

--- jd_joy_new.js
+++ jd_joy_new.js
@@ -311,53 +311,56 @@ class JDJRValidator {
 
   static jsonp(api, data = {}) {
     return new Promise((resolve, reject) => {
-      const fnId = `jsonp_${String(Math.random()).replace('.', '')}`;
-      const extraData = { callback: fnId };
-      const query = new URLSearchParams({ ...DATA, ...extraData, ...data }).toString();
-      const url = `http://${SERVER}${api}?${query}`;
-      const headers = {
-        Accept: '*/*',
-        'Accept-Encoding': 'gzip,deflate,br',
-        'Accept-Language': 'zh-CN,en-US',
-        Connection: 'keep-alive',
-        Host: SERVER,
-        'Proxy-Connection': 'keep-alive',
-        Referer: 'https://h5.m.jd.com/babelDiy/Zeus/2wuqXrZrhygTQzYA7VufBEpj4amH/index.html',
-        'User-Agent': UA,
-      };
-      const req = http.get(url, { headers }, (response) => {
-        let res = response;
-        if (res.headers['content-encoding'] === 'gzip') {
-          const unzipStream = new stream.PassThrough();
-          stream.pipeline(response, zlib.createGunzip(), unzipStream, reject);
-          res = unzipStream;
-        }
-        res.setEncoding('utf8');
-
-        let rawData = '';
-
-        res.on('data', (chunk) => (rawData += chunk));
-        res.on('end', () => {
-          try {
-            const ctx = {
-              [fnId]: (data) => (ctx.data = data),
-              data: {},
-            };
-
-            vm.createContext(ctx);
-            vm.runInContext(rawData, ctx);
-
-            // console.log(ctx.data);
-            res.resume();
-            resolve(ctx.data);
-          } catch (e) {
-            reject(e);
+      try {
+        const fnId = `jsonp_${String(Math.random()).replace('.', '')}`;
+        const extraData = { callback: fnId };
+        const query = new URLSearchParams({ ...DATA, ...extraData, ...data }).toString();
+        const url = `http://${SERVER}${api}?${query}`;
+        const headers = {
+          Accept: '*/*',
+          'Accept-Encoding': 'gzip,deflate,br',
+          'Accept-Language': 'zh-CN,en-US',
+          Connection: 'keep-alive',
+          Host: SERVER,
+          'Proxy-Connection': 'keep-alive',
+          Referer: 'https://h5.m.jd.com/babelDiy/Zeus/2wuqXrZrhygTQzYA7VufBEpj4amH/index.html',
+          'User-Agent': UA,
+        };
+        const req = http.get(url, { headers }, (response) => {
+          let res = response;
+          if (res.headers['content-encoding'] === 'gzip') {
+            const unzipStream = new stream.PassThrough();
+            stream.pipeline(response, zlib.createGunzip(), unzipStream, reject);
+            res = unzipStream;
           }
-        });
-      });
+          res.setEncoding('utf8');
 
-      req.on('error', reject);
-      req.end();
+          let rawData = '';
+
+          res.on('data', (chunk) => (rawData += chunk));
+          res.on('end', () => {
+            try {
+              const ctx = {
+                [fnId]: (data) => (ctx.data = data),
+                data: {},
+              };
+
+              vm.createContext(ctx);
+              vm.runInContext(rawData, ctx);
+
+              // console.log(ctx.data);
+              res.resume();
+              resolve(ctx.data);
+            } catch (e) {
+              reject(e);
+            }
+          });
+        });
+        req.on('error', reject);
+        req.end();
+      } catch (e) {
+        console.log('环境不支持');
+      }
     });
   }
 }
@@ -517,14 +520,12 @@ function injectToRequest(fn) {
   return (opts, cb) => {
     fn(opts, async (err, resp, data) => {
       if (err) {
-        console.error('Failed to request.');
+        console.error('Error: ', err);
         return;
       }
-
       if (data.search('验证') > -1) {
         console.log('JDJRValidator trying......');
         const res = await new JDJRValidator().run();
-
         opts.url += `&validate=${res.validate}`;
         fn(opts, cb);
       } else {
@@ -615,7 +616,12 @@ $.post = injectToRequest($.post.bind($));
           for (let t of tp.scanMarketList) {
             if (!t.status) {
               console.log('┖', t.marketName);
-              await doTask(JSON.stringify({ marketLink: `${t.marketLink || t.marketLinkH5}`, taskType: 'ScanMarket' }));
+              await doTask(
+                JSON.stringify({
+                  marketLink: `${t.marketLink || t.marketLinkH5}`,
+                  taskType: 'ScanMarket',
+                })
+              );
               await $.wait(5000);
             }
           }
@@ -664,7 +670,7 @@ function getFollowChannels() {
         },
       },
       (err, resp, data) => {
-        resolve($.toObj(data));
+        resolve(JSON.parse(data));
       }
     );
   });
@@ -737,8 +743,8 @@ function doTask(body, fnId = 'scan') {
         if (err) console.log('\tdoTask() Error:', err);
         try {
           console.log('\tdotask:', data);
-          data = $.toObj(data);
-          data.success ? console.log('\t任务成功') : console.log('\t任务失败', $.toStr(data));
+          data = JSON.parse(data);
+          data.success ? console.log('\t任务成功') : console.log('\t任务失败', JSON.stringify(data));
         } catch (e) {
           $.logErr(e);
         } finally {
@@ -775,7 +781,7 @@ function feed() {
         body: JSON.stringify({}),
       },
       (err, resp, data) => {
-        data = $.toObj(data);
+        data = JSON.parse(data);
         if (new Date().getTime() - new Date(data.data.lastFeedTime) < 10800000) {
           console.log('喂食间隔不够。');
           resolve();
@@ -804,8 +810,10 @@ function feed() {
             (err, resp, data) => {
               try {
                 // console.log('喂食', data)
-                data = $.toObj(data);
-                data.errorCode === 'feed_ok' ? console.log(`\t喂食成功！`) : console.log('\t喂食失败', $.toStr(data));
+                data = JSON.parse(data);
+                data.errorCode === 'feed_ok'
+                  ? console.log(`\t喂食成功！`)
+                  : console.log('\t喂食失败', JSON.stringify(data));
               } catch (e) {
                 $.logErr(e);
               } finally {
@@ -845,10 +853,10 @@ function award(taskType) {
       (err, resp, data) => {
         try {
           console.log('领取奖励', data);
-          data = $.toObj(data);
+          data = JSON.parse(data);
           data.errorCode === 'received'
             ? console.log(`\t任务成功！获得${data.data}狗粮`)
-            : console.log('\t任务失败', $.toStr(data));
+            : console.log('\t任务失败', JSON.stringify(data));
         } catch (e) {
           $.logErr(e);
         } finally {
@@ -884,8 +892,8 @@ function sign() {
       },
       (err, resp, data) => {
         try {
-          data = $.toObj(data);
-          data.success ? console.log(`\t签到成功！`) : console.log('\t签到失败！', $.toStr(data));
+          data = JSON.parse(data);
+          data.success ? console.log(`\t签到成功！`) : console.log('\t签到失败！', JSON.stringify(data));
         } catch (e) {
           $.logErr(e);
         } finally {
